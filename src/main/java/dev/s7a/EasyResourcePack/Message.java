f254fdeb0a5f02155e6ba4139c9686627fc0b343
sya-ri <sya79lua@gmail.com>
2021-08-15 00:45:59 +0900

    feat: /pack force を追加

--- src/main/java/dev/s7a/EasyResourcePack/Message.java
+++ src/main/java/dev/s7a/EasyResourcePack/Message.java
@@ -19,6 +19,29 @@ public class Message {
     public final static String OnlyPlayerCommandError = Prefix + ChatColor.RED + "プレイヤーのみが実行できるコマンドです";
 
     /**
+     * プレイヤー名を入力していなかった時に表示するエラー
+     */
+    public final static String NotEnterPlayerNameError = Prefix + ChatColor.RED + "プレイヤー名を入力してください";
+
+    /**
+     * プレイヤーが見つからなかった時に表示するエラー
+     *
+     * @param name プレイヤー名
+     */
+    public static String NotFoundPlayerNameError(String name) {
+        return Prefix + ChatColor.RED + name + " というプレイヤーは見つかりませんでした";
+    }
+
+    /**
+     * パックをプレイヤーに適用する前のメッセージ
+     *
+     * @param name プレイヤー名
+     */
+    public static String ApplyPackToPlayer(String name) {
+        return Prefix + ChatColor.WHITE + name + " のリソースパックを再読み込みします";
+    }
+
+    /**
      * コンフィグをリロードする前のメッセージ
      */
     public final static String ReloadCommand = Prefix + ChatColor.WHITE + "コンフィグをリロードします";
@@ -28,6 +51,8 @@ public class Message {
      */
     public final static List<String> CommandHelp = List.of(
             Prefix + ChatColor.WHITE + "コマンド一覧",
-            ChatColor.GRAY + "- /" + PackCommand.name + " " + PackCommand.Argument.Reload + ChatColor.GOLD + " コンフィグをリロードします"
+            ChatColor.GRAY + "- /" + PackCommand.name + ChatColor.GOLD + " リソースパックを再読み込みさせます",
+            ChatColor.GRAY + "- /" + PackCommand.name + " " + PackCommand.Argument.Reload + ChatColor.GOLD + " コンフィグをリロードします",
+            ChatColor.GRAY + "- /" + PackCommand.name + " " + PackCommand.Argument.Force + ChatColor.GOLD + " 指定したプレイヤーのリソースパックを再読み込みさせます"
     );
 }
--- src/main/java/dev/s7a/EasyResourcePack/PackCommand.java
+++ src/main/java/dev/s7a/EasyResourcePack/PackCommand.java
@@ -1,11 +1,13 @@
 package dev.s7a.EasyResourcePack;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.*;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -33,15 +35,18 @@ public class PackCommand implements CommandExecutor, TabCompleter {
 
     public static class Argument {
         public final static String Reload = "reload";
+        public final static String Force = "force";
 
         public final static List<String> all = List.of(
-                Reload
+                Reload,
+                Force
         );
     }
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        if (args.length < 1 || !sender.hasPermission(Permission.AdminCommand)) {
+        int argsLength = args.length;
+        if (argsLength < 1 || !sender.hasPermission(Permission.AdminCommand)) {
             if (sender instanceof Player) {
                 Player player = (Player) sender;
                 PackManager.applyToPlayer(player);
@@ -56,6 +61,28 @@ public class PackCommand implements CommandExecutor, TabCompleter {
                     Config.load();
                     break;
                 }
+                case Argument.Force: {
+                    if (argsLength < 2) {
+                        sender.sendMessage(Message.NotEnterPlayerNameError);
+                    } else {
+                        String arg1 = args[1];
+                        if (arg1.equals("@a")) {
+                            Collection<? extends Player> onlinePlayers = Bukkit.getOnlinePlayers();
+                            sender.sendMessage(Message.ApplyPackToPlayer("全プレイヤー(" + onlinePlayers.size() + ")"));
+                            onlinePlayers.forEach(PackManager::applyToPlayer);
+                        } else {
+                            Player targetPlayer = Bukkit.getPlayer(arg1);
+                            if (targetPlayer != null) {
+                                String targetPlayerName = targetPlayer.getName();
+                                sender.sendMessage(Message.ApplyPackToPlayer(targetPlayerName));
+                                PackManager.applyToPlayer(targetPlayer);
+                            } else {
+                                sender.sendMessage(Message.NotFoundPlayerNameError(arg1));
+                            }
+                        }
+                    }
+                    break;
+                }
                 default: {
                     sender.sendMessage(Message.CommandHelp.toArray(new String[0]));
                 }
@@ -64,16 +91,35 @@ public class PackCommand implements CommandExecutor, TabCompleter {
         return true;
     }
 
+    /**
+     * タブ補完を実行します
+     *
+     * @param arguments 全候補
+     * @param arg       入力中の引数
+     * @return 候補に一致する引数
+     */
+    private static List<String> complete(List<String> arguments, String arg) {
+        return arguments.stream().filter((a) -> a.startsWith(arg)).collect(Collectors.toList());
+    }
+
     @Nullable
     @Override
     public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String[] args) {
         if (sender.hasPermission(Permission.AdminCommand)) {
             int argsLength = args.length;
-            if (argsLength < 1) {
+            if (argsLength == 0) {
                 return Argument.all;
             } else if (argsLength == 1) {
                 String arg0 = args[0].toLowerCase();
-                return Argument.all.stream().filter((arg) -> arg.startsWith(arg0)).collect(Collectors.toList());
+                return complete(Argument.all, arg0);
+            } else if (argsLength == 2) {
+                String arg0 = args[0].toLowerCase();
+                if (arg0.equals(Argument.Force)) {
+                    Collection<? extends Player> onlinePlayers = Bukkit.getServer().getOnlinePlayers();
+                    List<String> onlinePlayerNames = onlinePlayers.stream().map(Player::getName).collect(Collectors.toList());
+                    onlinePlayerNames.add("@a");
+                    return complete(onlinePlayerNames, args[1]);
+                }
             }
         }
         return List.of();
